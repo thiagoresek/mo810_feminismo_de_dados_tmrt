36228109979ea99457864908d55db47c5bb31e4b
Thiago Resek <tresek@motorola.com>
2025-12-01 15:17:16 -0300

    Improve sidebar button visibility

--- main_page.py
+++ main_page.py
@@ -22,11 +22,26 @@ st.markdown("""
     [data-testid="stSidebar"] * {
         color: #ffffff !important;
     }
+    button[data-testid="stBaseButton-headerNoPadding"] [data-testid="stIconMaterial"] {
+        color: #FFFFFF !important;
+        fill: #FFFFFF !important;
+        -webkit-text-fill-color: #FFFFFF !important; /* icon font rendering fix */
+    }
+    button[data-testid="stExpandSidebarButton"] [data-testid="stIconMaterial"] {
+        color: #FFFFFF !important;
+        fill: #FFFFFF !important;
+        -webkit-text-fill-color: #FFFFFF !important; /* icon font rendering fix */
+    }
+    button[data-testid="stSidebarCollapseButton"] [data-testid="stIconMaterial"] {
+        color: #FFFFFF !important;
+        fill: #FFFFFF !important;
+        -webkit-text-fill-color: #FFFFFF !important; /* icon font rendering fix */
+    }
+</style>
 """, unsafe_allow_html=True)
 
 intro_page = st.Page("intro.py", title="Introdução")
 stories_page = st.Page("data_visualization.py", title="Histórias")
 
 pg = st.navigation([intro_page, stories_page])
-# st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
 pg.run()
